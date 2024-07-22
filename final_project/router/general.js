615f0512eb6ad31ee4c80227fe441a6fce02aec4
Phạm Đặng Hoàng Giang <phamdanghoanggiang@gmail.com>
2024-07-22 20:40:07 +0700

    Task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,7 +27,13 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let bookDetails = [];
+  for (const isbn of Object.keys(books)) 
+  {
+    if (books[isbn].author === author) bookDetails.push(books[isbn]);
+  }
+  return res.send(bookDetails);
 });
 
 // Get all books based on title
