7e860f1fb842dba7f0fd639cb693203f98c3d303
Phạm Đặng Hoàng Giang <phamdanghoanggiang@gmail.com>
2024-07-23 15:12:26 +0700

    Task 6

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,6 +7,7 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+  return users.findIndex((user) => user.username === username) === -1;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
